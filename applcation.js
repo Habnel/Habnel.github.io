4d8db34463aa390f427c785e6b84231827891e56
Habnel <HAcevedo@kippnyccp.org>
2017-06-09 13:12:27 -0400

    Update applcation.js

--- applcation.js
+++ applcation.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
 	var random_num = Math.floor((Math.random()*100) + 1);
-	//create click event for submit button
- 
- 
- 
+	$("button").click(function(){
+		var name = $("imput[name=first_name]").val();
+		$("#Welcome").html(name);
+}
+
+
+
 	//create click event for clear button
- 
- 
+
+
 });
 
