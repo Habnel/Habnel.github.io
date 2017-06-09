97686d1e128d04950d3b2457a3209849256729b4
Habnel <HAcevedo@kippnyccp.org>
2017-06-09 13:31:16 -0400

    Create application.js

--- application.js
+++ application.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
 	var random_num = Math.floor((Math.random()*100) + 1);
-	$("button").click(function(){
-		var name = $("imput[name=first_name]").val();
-		$("#Welcome").html(name);
-}
+	
+	$("#Submit").click(function(){
+		var guess = $("input[name=guess]").val();
+		if(guess == random_num){
+			$('div').html("You guessed correctly!");
+		}
+		else if(guess < random_num){
+			$('div').html("You guessed too low. Bring it Up!");
+		}
+		else {
+			$('div').html("You guessed too high. Lower it!");
+		}
+	});
 
 
 
